578558b6f98223a71db3297d7fa97ce9f0e157c5
Jan <jan.fuchsmann@gmail.com>
2025-01-07 19:20:48 +0100

    Add missing files from tar

---
+++ functions/prompts_custom.php
@@ -0,0 +1,13 @@
+<?php
+
+// Functions to be provided to OpenAI
+
+$ENABLED_FUNCTIONS=[
+    //'OpenInventory',
+    'SheatheWeapon',
+    'SetCurrentTask',
+    'ReadQuestJournal'
+	//'Attack'
+];
+
+?>
